0906795c2008854802d8dea61aabc71665dd32d7
karan-95S <119615883+karan-95S@users.noreply.github.com>
2023-12-07 11:53:11 +0100

    Update carte.js
    rajout du popup et de tout les points, manque de compléter les informations dans le popup et mettre en forme

--- carte/carte.js
+++ carte/carte.js
@@ -18,3 +18,23 @@ paris.addEventListener("click", () => {
 marseille.addEventListener("click", () => {
     map.setView([43.2965, 5.3698], 12);
 });
+
+
+fetch('data_asso.geojson')
+    .then(response => response.json())
+    .then(data => {
+        // Parcourir chaque feature dans le GeoJSON
+        data.features.forEach(feature => {
+            // Récupérer les coordonnées de latitude et longitude du point
+            var latitude = feature.geometry.coordinates[1];
+            var longitude = feature.geometry.coordinates[0];
+
+            // Créer un marqueur pour chaque point
+            var marker = L.marker([latitude, longitude]).addTo(map);
+
+            if (feature.properties && feature.properties.nom) {
+                marker.bindPopup(feature.properties.nom);
+            }
+
+        });
+    });
