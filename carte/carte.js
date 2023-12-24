51d23edfa793b8183ab9c933b7ea3bedd13cbb71
karan-95S <119615883+karan-95S@users.noreply.github.com>
2023-12-24 11:07:20 +0100

    Update carte.js

--- carte/carte.js
+++ carte/carte.js
@@ -1,7 +1,31 @@
-var map = L.map('map').setView([48.866667, 2.333333], 5);
+var map = L.map("map").setView([48.856614, 2.3522219], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+var Stadia_OSMBright = L.tileLayer(
+    "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+    {
+        maxZoom: 20,
+        attribution:
+            '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }
+);
 
+
+fetch('data_asso.geojson')
+    .then(response => response.json())
+    .then(data => {
+        // Parcourir chaque feature dans le GeoJSON
+        data.features.forEach(feature => {
+            // Récupérer les coordonnées de latitude et longitude du point
+            var latitude = feature.geometry.coordinates[1];
+            var longitude = feature.geometry.coordinates[0];
+
+            // Créer un marqueur pour chaque point
+            var marker = L.marker([latitude, longitude]).addTo(map);
+
+            if (feature.properties && feature.properties.nom) {
+                marker.bindPopup(feature.properties.nom);
+            }
+
+        });
+    });
+Stadia_OSMBright.addTo(map);
